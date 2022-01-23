500d39fb5b05c2e6cb8b7aaecdd14fa28d9acfd5
Floribert <floribertnt@gmail.com>
2022-01-23 09:26:07 +0100

    add netlify cms

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,24 @@
 import * as React from "react"
 import PropTypes from "prop-types"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+const Header = () => (
+  <header>
+   <Navbar bg="dark" variant="dark">
+    <Container>
+    <Nav className="justify-content-end">
+      <Nav.Link href="#">Accueil</Nav.Link>
+      <Nav.Link href="gallerie">Gallerie</Nav.Link>
+      <Nav.Link href="#pricing">Tarifs et specifications</Nav.Link>
+      <Nav.Link href="#pricing">Contact</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
   </header>
 )
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,11 @@
 import * as React from "react"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const NotFoundPage = () => (
-  <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  return(
+  <div>
+    <p>404</p>
+  </div>
+  )
+}
 
 export default NotFoundPage
